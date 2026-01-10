cbfddced11887a5a837f42b663f0f65127b492a0
Jeslyn Lam <jeslynlxy@hotmail.com>
2026-01-10 16:08:46 +0800

    fix: update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { Box, Text } from "@chakra-ui/react";
-
 function App() {
   return (
-    <Box
-      minH="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center"
-      fontSize="xl"
-    >
-      <Text>Brainrot</Text>
-    </Box>
+    <div >
+      <h1>Brainrot</h1>
+    </div>
   );
 }
 
