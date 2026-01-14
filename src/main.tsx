c3bb27bc1ba9adff84584fa0b7847c7511ceec1d
Jeslyn Lam <jeslynlxy@hotmail.com>
2026-01-14 19:58:02 +0800

    feat: render Space Grotesk from local font files

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./fonts.css";
 
 const rootElement = document.getElementById("root");
 
